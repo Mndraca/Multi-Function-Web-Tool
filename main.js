9da03b1f799cef7d1382342c2607a6380d063edd
Mndraca <marizanovic3@gmail.com>
2024-04-18 11:41:26 -0400

    second commit

---
+++ main.js
@@ -0,0 +1,37 @@
+//let  factorialN = document.getElementById('factorialN');
+//let  reverseWord = document.getElementById('reverse');
+//let  upperC = document.getElementById('upper');
+//let  loop = document.getElementById('loop');
+//const btn = document.getElementById("userInput");
+ //const out1 = document.getElementById("output");
+ // function fun1 (){
+ //out1.innerHTML = btn.value;
+//  }
+//btn.addEventListener('click', fun1);
+
+//function addEntry() {
+  //  const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
+  
+ // }
+ const submit = document.getElementById("submit");
+ const inputBox = document.getElementById("userInput");
+ const output = document.getElementById("output");
+
+
+ //submit.addEventListener (`click`, function(){
+ // let paragraph = document.createElement('p')
+ // paragraph.innerText = inputBox.value
+ // inputBox.appendChild(paragraph);
+
+
+ //})
+
+ function addTask (){
+   if (inputBox.value === ' '){
+      alert ("You must write something!")
+   } 
+ };
+
+ console.log(addTask ());
+
+
